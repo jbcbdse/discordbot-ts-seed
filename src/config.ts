963634df78bc7e7750d6522787e6a41c9ef65785
Jon Barnett <jonbarnett@gmail.com>
2019-08-13 18:23:15 -0500

    Add basic logic for seed
    Add Discord.js
    Add dotenv with ignored .env file
    Add basic logic for handling messages

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const config = {
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import * as Discord from 'discord.js';
+import { config } from './config';
+
 console.log('hello world!');
+
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+  // our bot needs to know if it will execute a command
+  // it will listen for messages that will start with `!`
+  if (message.content.substring(0, 1) === '!') {
+    const args = message.content.substring(1).split(' ');
+    const cmd = args.shift();
+
+    if (cmd === 'ping') {
+      message.reply('Pong!');
+    }
+  }
+});
+
+bot.login(config.DISCORD_TOKEN).then(() => {
+  console.log(`Logged in as ${bot.user.tag}`);
+});
